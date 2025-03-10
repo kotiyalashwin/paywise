236cb12dcdd6d4b88f37b47b0f47efff6ecfa92d
ashwink02 <ashwinkotiyal07@gmail.com>
2025-03-10 22:16:24 +0530

    bankwebhook setup

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,47 @@
+import express from "express";
+import db from "@repo/db/client";
+
+const app = express();
+app.use(express.json());
+
+app.post("/hdfcwebhook", async (req, res) => {
+  //zod validations
+  const paymentInfor = {
+    token: req.body.token,
+    userId: Number(req.body.user_identifier),
+    amount: req.body.amount,
+  };
+
+  //either both of the following happen or none happens -> use PrismaTransactions
+
+  try {
+    await db.$transaction([
+      db.balances.update({
+        where: {
+          userId: paymentInfor.userId,
+        },
+        data: {
+          amount: {
+            increment: paymentInfor.amount,
+          },
+        },
+      }),
+      db.onRampTransaction.update({
+        where: {
+          token: paymentInfor.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
+    //the following response is MANDATORY to let hdfc server to know we got the transaction
+    res.status(200).json({
+      message: "captured",
+    });
+  } catch (e) {
+    res.status(411).json({
+      message: "transaction failed",
+    });
+  }
+});
